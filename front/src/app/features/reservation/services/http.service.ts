b852d8c7ff8adffb27cdcf545be87ff6960041cd
duke <webcreation33@yahoo.fr>
2022-05-20 08:04:31 +0200

    AV12

--- front/src/app/features/reservation/services/http.service.ts
+++ front/src/app/features/reservation/services/http.service.ts
@@ -18,13 +18,13 @@ export class HttpService {
   saveReservations(reservation: ReservationSave): Observable<any> {
     const headers = { 'content-type': 'application/json'}
     const body = JSON.stringify(reservation);
-    return this.http.post(`${environment.url}/data/reservations`, body, {'headers':headers , observe: 'response'}).pipe(
+    return this.http.post(`${environment.url}/api/reservations`, body, {'headers':headers , observe: 'response'}).pipe(
       catchError(this.handleError)
     );
   }
 
   getLocations(): Observable<number[]> {
-    return this.http.get<number[]>(`${environment.url}/data/locations`).pipe(
+    return this.http.get<number[]>(`${environment.url}/api/locations`).pipe(
       catchError(this.handleError),
     );
   }
@@ -36,26 +36,26 @@ export class HttpService {
   }
 
   getReservations(foodtrackId: number, societyId: number): Observable<ReservationDto[]> {
-    return this.http.get<ReservationDto[]>(`${environment.url}/data/reservations/${foodtrackId}/${societyId}`).pipe(
+    return this.http.get<ReservationDto[]>(`${environment.url}/api/reservations/${foodtrackId}/${societyId}`).pipe(
       catchError(this.handleError)
     );
   }
 
   getReservationsFromSociety(societyId: number, date: string): Observable<{locationNo: number}[]> {
-    return this.http.get<{locationNo: number}[]>(`${environment.url}/data/reservations-society/${societyId}/${date}`).pipe(
+    return this.http.get<{locationNo: number}[]>(`${environment.url}/api/reservations-society/${societyId}/${date}`).pipe(
       catchError(this.handleError)
     );
   }
 
   getSocieties(): Observable<SocietyDto[]> {
 
-    return this.http.get<SocietyDto[]>(`${environment.url}/data/societies`).pipe(
+    return this.http.get<SocietyDto[]>(`${environment.url}/api/societies`).pipe(
       catchError(this.handleError)
     );
   }
 
   getFoodtracks(): Observable<FoodtrackDto[]> {
-    return this.http.get<FoodtrackDto[]>(`${environment.url}/data/foodtracks`).pipe(
+    return this.http.get<FoodtrackDto[]>(`${environment.url}/api/foodtracks`).pipe(
       catchError(this.handleError)
     );
   }
